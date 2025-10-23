9d315fb53465462377599a248cf1f9b6dabe5565
aliced1 <alice.duff5@gmail.com>
2025-10-22 20:25:41 -0400

    Switching 0 and 1, changing some keys to one shots, and adding a text file to remember what the compile command is

--- keyboards/crkbd/keymaps/aliced1/keymap.c
+++ keyboards/crkbd/keymaps/aliced1/keymap.c
@@ -57,11 +57,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      OS_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_RSFT,
+      OS_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, OS_RSFT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      OS_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_LCTL,
+      OS_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, OS_LCTL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                OS_LALT, KC_ENT, LT(1,KC_TAB),             LT(2,KC_BSPC),KC_SPC,KC_RALT
+                                OS_LALT, KC_ENT, LT(1,KC_TAB),             LT(2,KC_BSPC),KC_SPC,OS_LALT
                                       //`--------------------------'  `--------------------------'
 
   ),
@@ -71,12 +71,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,---------------------------------------------------------------------------           ,-----------------------------------------------------.
       KC_TRNS, KC_HOME, LCS(KC_TAB), KC_UP ,       LCTL(KC_TAB),  KC_PGUP,                KC_NO,   KC_7,    KC_8,    KC_9,    KC_PMNS,  KC_TRNS,
   //|--------+--------+-------------+-------------+--------------+--------------|        |--------+--------+--------+--------+--------+--------|
-      KC_TRNS, KC_END,  KC_LEFT,     KC_DOWN,      KC_RIGHT,      KC_PGDN,                KC_DOT,  KC_4,    KC_5,    KC_6,    KC_PPLS,  KC_TRNS,
+      KC_TRNS, KC_END,  KC_LEFT,     KC_DOWN,      KC_RIGHT,      KC_PGDN,                KC_0,    KC_4,    KC_5,    KC_6,    KC_PPLS,  KC_TRNS,
   //|--------+--------+-------------+-------------+--------------+--------------|        |--------+--------+--------+--------+--------+--------|
-      KC_TRNS, KC_NO,   C(KC_X),     C(KC_C),      C(KC_V),       KC_NO,                  KC_0,    KC_1,    KC_2,    KC_3,    KC_EQL,   KC_TRNS,
+      KC_TRNS, KC_NO,   C(KC_X),     C(KC_C),      C(KC_V),       KC_NO,                  KC_DOT,  KC_1,    KC_2,    KC_3,    KC_EQL,   KC_TRNS,
   //|--------+--------+-------------+-------------+--------------+--------------|        |--------+--------+--------+--------+--------+--------|
-                                          KC_TRNS,KC_TRNS,KC_TRNS,      LT(3,KC_BSPC),KC_TRNS,KC_TRNS
-                                      //`--------------------------'  `--------------------------'
+                                                          KC_TRNS,KC_TRNS,KC_TRNS,      LT(3,KC_BSPC),KC_TRNS,KC_TRNS
+                                                      //`--------------------------'  `--------------------------'
   ),
 
     // Symbol Layer
