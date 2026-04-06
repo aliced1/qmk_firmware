52dff0582a0f09de482731d4282ee0aa1faaa942
alice <alice.duff5@gmail.com>
2026-04-06 10:32:08 -0400

    Removing one macro and adding a shift-insert macro

--- keyboards/crkbd/keymaps/aliced1/keymap.c
+++ keyboards/crkbd/keymaps/aliced1/keymap.c
@@ -97,9 +97,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_F6,                          KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS, KC_NO,   KC_MRWD, KC_MFFD, KC_MPLY, KC_VOLU,                      KC_BRIU, LCS(KC_V),DM_REC1, DM_REC2, DM_RSTP, KC_TRNS,
+      KC_TRNS, KC_NO,   KC_MRWD, KC_MFFD, KC_MPLY, KC_VOLU,                      KC_BRIU, LCS(KC_V),LSFT(KC_INS), DM_REC2, DM_RSTP, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS, OS_LGUI, KC_NO,   KC_NO,   KC_MSTP, KC_VOLD,                      KC_BRID, LSG(KC_S),DM_PLY1, DM_PLY2, KC_NO, KC_TRNS,
+      KC_TRNS, OS_LGUI, KC_NO,   KC_NO,   KC_MSTP, KC_VOLD,                      KC_BRID, LSG(KC_S),KC_NO, DM_PLY2, KC_NO, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_TRNS,KC_TRNS,KC_TRNS,      KC_TRNS,KC_TRNS,KC_TRNS
                                       //`--------------------------'  `--------------------------'
